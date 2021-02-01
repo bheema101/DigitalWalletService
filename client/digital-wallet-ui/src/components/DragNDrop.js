da853f811a1d15215c4b7d92709a334e785c89b6
Riya Binny <a-9089@LE1662.local>
2021-02-01 11:49:53 +0530

    preview_and_security

--- client/digital-wallet-ui/src/components/DragNDrop.js
+++ client/digital-wallet-ui/src/components/DragNDrop.js
@@ -7,25 +7,73 @@ class DragNDrop extends Component {
   state = {
     id: "",
     files: [],
+    preview: "",
+    downloadFileType: "",
+    tuid: ""
   };
   base_url = "https://localhost/dws";
   componentDidMount() {
     //getAllFiles
-    console.log("Mounted");
     axios({
       method: "get",
       url: `${this.base_url}/getAllFiles/PN101/TU102`,
     })
       .then((response) => {
+        
         const data = response.data[0]
         console.log(response);
-        this.setState({ files: data.fileNames, id: data.id });
+        this.setState({ files: data.fileNames, id: data.id, tuid: data.tuid });
       })
       .catch((err) => console.log(err));
   }
 
-  onDropFile = (files, event) => {
+  fileTypeChecker = file => new Promise((resolve) => {
+    const fileReader = new FileReader()
+  
+    fileReader.onloadend = (e) => {
+      const arr = (new Uint8Array(e.target.result)).subarray(0, 4)
+      let header = ''
+      let type = null
+      for (let i = 0; i < arr.length; i++) {
+        header += arr[i].toString(16)
+      }
+      console.log(header);
+      switch (header) {
+        case '89504e47': // png
+        case 'ffd8ffe0': // following are jpeg magic numbers
+        case 'ffd8ffe1':
+        case 'ffd8ffe2':
+        case 'ffd8ffe3':
+        case 'ffd8ffe8':
+          type = 'IMG'
+          break
+        case 'd0cf11e0': // xls
+        case '504b34':   // xlsx
+          type = 'XLS'
+          break
+        case '25504446':
+          type = 'PDF'
+          break
+        case '23206874':
+        case '48656c6c':
+        case '54686973':
+          type = 'TEXT'
+          break
+        default:
+          type = 'unknown' 
+          break
+      }
+      resolve(type)
+    }
+  
+    fileReader.readAsArrayBuffer(file.slice(0, 4))
+  })
+
+  onDropFile = async (files, event) => {
+    const fileType = await this.fileTypeChecker(files[0]);
+    console.log(fileType);
     console.log(files[0]);
+    if(fileType === 'IMG' || fileType === 'TEXT' || fileType === 'PDF') { 
     const formdata = new FormData();
     formdata.append("file", files[0]);
     formdata.append("tuid", "TU102");
@@ -48,8 +96,55 @@ class DragNDrop extends Component {
           })
           .catch((err) => console.log(err));
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err));}
+      else {
+        alert("This file is not allowed!!!")
+      }
   };
+
+  onPreview(file, id, tuid) {
+    console.log(file);
+    
+    const FileDownload = require("js-file-download");
+    axios({
+      method: "post",
+      url: `${this.base_url}/download`,
+      data: { fileId: id, fileName: file, tuid: tuid },
+      responseType : "blob"
+    })
+      .then((response) => {
+        console.log(response);
+        if(response.headers["content-type"]==="image/png") {
+          this.setState({preview:response.data, downloadFileType:response.headers["content-type"]});
+        }
+        else{
+        const reader = new FileReader();
+
+    reader.addEventListener('loadend', (e) => {
+      const text = e.srcElement.result;
+      this.setState({preview:text, downloadFileType:response.headers["content-type"]});
+    });
+
+    reader.readAsText(response.data);}
+      })
+      .catch((err) => console.log(err));
+  }
+
+  downloadFile(file, id, tuid) {
+    console.log(file);
+    const FileDownload = require("js-file-download");
+    axios({
+      method: "post",
+      url: `${this.base_url}/download`,
+      data: { fileId: id, fileName: file, tuid: tuid },
+    })
+      .then((response) => {
+        console.log(response);
+        FileDownload(response.data, file);
+        console.log("File downloaded successfully");
+      })
+      .catch((err) => console.log(err));
+  }
 
   render() {
     return (
@@ -57,23 +152,28 @@ class DragNDrop extends Component {
         <h1>Digital Wallet Demo</h1>
         <div>
           <FileDrop onDrop={(files, event) => this.onDropFile(files, event)}>
-
             {this.state.files.length > 0
-              ? this.state.files.map((file) => (
-                  <a
-                    href={`${this.base_url}/download?fileId=${this.state.id}&fileName=${file}`}
-                    key={file}
-                  >
-                    {file}
-                    <br />
-                  </a>
+              ? this.state.files.map((file) => (<div key={file}>
+                 <div
+                 onClick={() =>
+                   this.onPreview(file, this.state.id, this.state.tuid)
+                 }
+                 key={file}
+              >
+                {file}  
+                <br />
+              </div><button onClick={() => this.downloadFile(file, this.state.id, this.state.tuid)}>Download</button></div>
                 ))
               : `Drop some files here!`}
+              <br />
+              <br />
+              <br />
+              {this.state.downloadFileType === "image/png"  ? <img src={URL.createObjectURL(this.state.preview)} style={{height:"300px", width:"300px"}}/>:
+              <div>{this.state.preview}</div>} 
           </FileDrop>
         </div>
       </div>
     );
   }
 }
-
 export default DragNDrop;
